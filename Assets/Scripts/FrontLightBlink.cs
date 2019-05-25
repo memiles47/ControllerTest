7e2fa6d17b040b9fc4ba0d1da38478325bfdfd7a
Michael E Miles <michael@miles-me.net>
2019-05-25 14:26:03 -0500

    Changes to blinking script

--- Assets/Scripts/FrontLightBlink.cs
+++ Assets/Scripts/FrontLightBlink.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class FrontLightBlink : MonoBehaviour
 {
-    //This script is to start the front lights to blink
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
